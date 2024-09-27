1c5e034e299618a432141dfa89ab1442970309c2
Saskia Brienesse-Little (1000125002) <BRIESG1@student.op.ac.nz>
2024-09-27 14:30:17 +1200

    Image added

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,18 +1,28 @@
 <script>
     export let year = 2024;
+    export let pic;
+    export let picname;
 
     let currentTime = new Date();
 </script>
 
-<p>&copy; Web 1 {year}: the time is currently: {currentTime}</p>
+<div>
+    <img src={pic} alt={picname}>
+    <p>&copy; Web 1 {year}: the time is currently: {currentTime}</p>
+</div>
+
+
 
 <style>
+    div {
+        display: flex;
+        flex-direction:column;
+    }
     p {
         background-color: salmon;
         padding: 2em;
         font-weight: bold;
         font-size:x-small;
         border: 10px dotted bisque;
-        margin-right: 135em;
     }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,12 @@
 <script>
     import Footer from '$lib/Footer.svelte';
+    import catpic from '$lib/cat.jpg';
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<Footer year={2022}/>
+<Footer year={2022} pic={catpic} picname={"cat"}/>
 <Footer year={2023}/>
 <Footer year={1980}/>
 

96c2113e5727320f4e727a4f9aa497a86e051902
Saskia Brienesse-Little (1000125002) <BRIESG1@student.op.ac.nz>
2024-09-27 14:08:40 +1200

    Practice for components

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,18 @@
+<script>
+    export let year = 2024;
+
+    let currentTime = new Date();
+</script>
+
+<p>&copy; Web 1 {year}: the time is currently: {currentTime}</p>
+
+<style>
+    p {
+        background-color: salmon;
+        padding: 2em;
+        font-weight: bold;
+        font-size:x-small;
+        border: 10px dotted bisque;
+        margin-right: 135em;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
+<script>
+    import Footer from '$lib/Footer.svelte';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Footer year={2022}/>
+<Footer year={2023}/>
+<Footer year={1980}/>
+
+<style>
+    :global(*) {
+        font-family: sans-serif;
+    }
+</style>

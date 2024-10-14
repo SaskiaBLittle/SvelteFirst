cbd09e31af856443505c590dd952a5b498279ab7
SaskiaBLittle <briesg1@student.op.ac.nz>
2024-10-15 10:58:44 +1300

    display the header component on the home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,13 @@
 <script>
+    import Header from '$lib/Header.svelte';
     import Footer from '$lib/Footer.svelte';
     import catpic from '$lib/cat.jpg';
 </script>
 
-<h1>Welcome to SvelteKit</h1>
+<Header />
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<Footer year={2022} pic={catpic} picname={"cat"}/>
+<Footer year={2022} pic={catpic} picname={"cat"} />
 <!-- <Footer year={2023}/>
 <Footer year={1980}/> -->
 

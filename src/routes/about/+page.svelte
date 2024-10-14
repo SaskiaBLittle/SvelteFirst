96ad7c5f39234e3dc69ab8729b1b28618da440ae
SaskiaBLittle <briesg1@student.op.ac.nz>
2024-10-15 11:20:41 +1300

    Added header and footer to the about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,8 +1,11 @@
 <script>
-
+    import Header from '$lib/Header.svelte';
+    import Footer from '$lib/Footer.svelte';
 </script>
 
+<Header headingTitle="About Page" />
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia assumenda ducimus molestiae dicta, esse libero sint nesciunt tempora eum quidem obcaecati quaerat facilis deleniti odit, consequatur nemo magni ea facere.</p>
+<Footer year={2022} />
 
 <style>
 

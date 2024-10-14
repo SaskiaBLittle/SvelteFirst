9b4b340bcaab21081f7082bde8d1634ebfdd5dfc
SaskiaBLittle <briesg1@student.op.ac.nz>
2024-10-15 11:17:56 +1300

    Set up the about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+
+</script>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia assumenda ducimus molestiae dicta, esse libero sint nesciunt tempora eum quidem obcaecati quaerat facilis deleniti odit, consequatur nemo magni ea facere.</p>
+
+<style>
+
+</style>
